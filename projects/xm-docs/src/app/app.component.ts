e23cae2da5525cd1722c33d3c62b546be67c2c7f
lycHub <1120805364@qq.com>
2022-05-15 03:38:33 +0800

    ci

--- projects/xm-docs/src/app/app.component.ts
+++ projects/xm-docs/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="app-root">
-      <p class="text-3xl font-bold underline">撒</p>
+      <p class="text-3xl font-bold underline">撒66</p>
       <div class="demo-box">
         <a
           xmBtn
